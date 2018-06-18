2564d0c9aeb6b34167189d966b2275525c643224
muttley <mattiello@gmail.com>
2018-06-18 14:49:41 +0200

    fix tag

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 docker build --no-cache=true -t muttley/xenial-php-5.6-dev .
-docker tag muttley/xenial-php-5.6 muttley/xenial-php-5.6-dev:latest-local
+docker tag muttley/xenial-php-5.6-dev muttley/xenial-php-5.6-dev:latest-local
 docker push muttley/xenial-php-5.6-dev:latest-local
